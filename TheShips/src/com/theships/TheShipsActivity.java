0391bf325031df31a854d6f85738f3e8795deb38
slaypawel@gmail.com <slaypawel@gmail.com@3ebb0992-2993-63f1-b2b7-f11973a2219f>
2011-12-04 19:09:13 +0000

    Dorobione dzialanie przycisku wstecz

--- TheShips/src/com/theships/TheShipsActivity.java
+++ TheShips/src/com/theships/TheShipsActivity.java
@@ -7,28 +7,54 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class TheShipsActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override    
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-        						WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        setContentView(R.layout.main);
-    }
-    public void main_to_choose(View w)
-    {
-    	setContentView(R.layout.gamerchoose);
-    }
-    public void singleplayer_to_difficulty(View w)
-    {
-    	setContentView(R.layout.difficultychoose);
-    }
-    public void statistics(View w)
-    {
-    	setContentView(R.layout.statistics);
-    }
-} 
+	static int currentview = R.layout.main;
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setContentView(R.layout.main);
+	}
+
+	public void main_to_choose(View w) {
+		currentview = R.layout.gamerchoose;
+		setContentView(currentview);
+	}
+
+	public void singleplayer_to_difficulty(View w) {
+		currentview = R.layout.difficultychoose;
+		setContentView(currentview);
+	}
+
+	public void statistics(View w) {
+		currentview = R.layout.statistics;
+		setContentView(currentview);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		switch(currentview) {
+		case R.layout.gamerchoose :
+			currentview = R.layout.main;
+			setContentView(currentview);
+			break;
+		case R.layout.difficultychoose :
+			currentview = R.layout.gamerchoose;
+			setContentView(currentview);
+			break;
+		case R.layout.statistics :
+			currentview = R.layout.main;
+			setContentView(currentview);
+			break;
+		case R.layout.main :
+			finish();
+			break;
+		}
+		return;
+	}
+}
