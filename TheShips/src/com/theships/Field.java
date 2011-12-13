a780e89b89c25b492c6221fc4ef006954951fe24
slaypawel <slaypawel@gmail.com>
2011-12-13 00:01:43 +0000

    updates

--- TheShips/src/com/theships/Field.java
+++ TheShips/src/com/theships/Field.java
@@ -1,6 +1,7 @@
 package com.theships;
 
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Field {
@@ -12,16 +13,59 @@ public class Field {
 	public static final int _ship = 1;
 	public static final int _missed = 2;
 	public static final int _shot = 3;
+	public static final int _sink = 4;
+	
+	public Button getButton() {
+		return this.button;
+	}
 	
 	public Field(View v, int nr, int state) {
-		this.button = (Button)v;
 		this.nr = nr;
-		this.state = state;
+		setButton(v);
+		setstate(state);
 	}
 	
 	public Field(View v, int nr) {
-		this.button = (Button)v;
+		setButton(v);
 		this.nr = nr;
-		this.state = _empty;
+		setstate(Field._empty);
+	}
+	
+	public void setButton(View v) {
+		this.button = (Button)v;
+		this.button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if(getstate()==Field._empty) {
+					setstate(Field._missed);
+				}
+				if(getstate()==Field._ship) {
+					setstate(Field._shot);
+				}
+			}
+		});
+	}
+	
+	public int getstate() {
+		return state;
+	}
+	
+	public void setstate(int s) {
+		int t = R.drawable.regular;
+		this.state = s;
+		switch(s) {
+		case Field._empty :
+			t = R.drawable.regular;
+			break;
+		case Field._missed :
+			t = R.drawable.pudlo;
+			break;
+		case Field._shot :
+			t = R.drawable.trafiony;
+			break;
+		case Field._sink :
+			t = R.drawable.zatopiony;
+			break;
+		}
+		this.button.setBackgroundResource(t);
 	}
 }
--- TheShips/src/com/theships/Ship.java
+++ TheShips/src/com/theships/Ship.java
@@ -1,7 +1,42 @@
 package com.theships;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+
 public class Ship {
 	private Field[] fields;
 	private int length;
 	private boolean isSink;
+	
+	public Ship(int l, View[] v, int[] nr) {
+		this.length= l;
+		this.fields = new Field[this.length];
+		for(int i = 0; i < this.length; i++) {
+			fields[i] = new Field(v[i], nr[i], Field._ship);
+		}
+		this.isSink = false;
+	/*	fields[0].getButton().setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				for(int i = 0; i < getLength(); i++) {
+					fields[i].setstate(Field._sink)
+				}
+			}
+		});*/
+	}
+	
+	public int getLength() {
+		return this.length;
+	}
+	
+	public void updateState() {
+		for(int i = 0; i < this.length; i++) {
+			if(fields[i].getstate() != Field._shot) 
+				this.isSink = false;
+		}
+		this.isSink = true;
+	}
+	
+	public boolean getLiveState() {
+		return !isSink;
+	}
 }
