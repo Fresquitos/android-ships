7561b74074c0850a78654aec5522d9e85d37eec0
zielinska.amelia <zielinska.amelia@gmail.com>
2012-01-10 14:05:54 +0000

    hard mołd

--- TheShips/src/com/theships/AIPlayer.java
+++ TheShips/src/com/theships/AIPlayer.java
@@ -7,11 +7,50 @@ import android.view.View;
 
 public class AIPlayer extends Player {
 	private int[][] shots = new int[10][10];
+	private int[][] moves;
 	public static final int _easy = 0;
 	public static final int _hard = 1;
 	
 	public AIPlayer(int[] parsedGrid, View[] rids, boolean fake) {
 		super(parsedGrid, rids, fake);
+		moves = new int[4][14];
+		int rand = new Random().nextInt(4);
+		int temp = moves[0][0] = rand;
+		int prev = temp;
+		for(int j = 0; j < 2; j++) {
+			for (int i = 1; i < 13; i++) {
+				if((moves[j][i-1]%10 + 4) > 9) {
+					temp += 20;
+					temp = ((temp%10 - 2) >= 0) ? temp - 2 : temp + 2;
+					moves[j][i] = temp;
+				} else
+					moves[j][i] = moves[j][i-1] + 4;
+				if(moves[j][i]>=100)
+					moves[j][i] = -1;
+			}
+			moves[j + 1][0] = ((prev+11)%10 - 4) >=0 ? prev + 7 : prev + 11;
+			prev = moves[j + 1][0];
+			temp = prev;
+		}
+		prev = (moves[0][0] - 2) >= 0 ? moves[0][0] - 2 : moves[0][0] + 2;
+		temp = moves[2][0] = prev;
+		for(int j = 2; j < 4; j++) {
+			for (int i = 1; i < 13; i++) {
+				if((moves[j][i-1]%10 + 4) > 9) {
+					temp += 20;
+					temp = ((temp%10 - 2) >= 0) ? temp - 2 : temp + 2;
+					moves[j][i] = temp;
+				} else
+					moves[j][i] = moves[j][i-1] + 4;
+				if(moves[j][i]>=100)
+					moves[j][i] = -1;
+			}
+			if(j != 3) {
+				moves[j + 1][0] = ((prev+11)%10 - 4) >=0 ? prev + 7 : prev + 11;
+				prev = moves[j + 1][0];
+				temp = prev;
+			}
+		}
 	}
 
 	public AIPlayer(View[] views2) {
@@ -50,6 +89,7 @@ public class AIPlayer extends Player {
 		}
 		if(mode == _hard) {
 			//hard mode
+			
 		}
 	}
 	public void winAI (Activity a) {
